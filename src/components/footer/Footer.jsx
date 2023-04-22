d75bbb41efa2e6764980c5bf730e3558c5d8044c
hossainnh0 <87042921+hossainnh0@users.noreply.github.com>
2023-04-22 23:51:36 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,12 +5,8 @@ const Footer = () =>{
     return (
         <div className='footer container'>
             <div className='footer__disb'>
-            Copyright ThangYuu © 2022
-            </div>
-            <div className='social'>
-            <a href='https://www.facebook.com/03668382xx'><FacebookFilled /></a>
-            <a href='https://github.com/yukithang13'><GithubOutlined /></a>
-            </div>
+            Copyright HOFLIX © 2022
+
         </div>
     );
 }
